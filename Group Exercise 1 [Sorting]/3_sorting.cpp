d628fd6eb19a82fd28bd9d5b1e5554ccfbec8334
vhtuananh020402 <vhtuananh020402@gmail.com>
2022-03-24 17:14:45 +0700

    update a comment
    I just update a comment

--- Group Exercise 1 [Sorting]/3_sorting.cpp
+++ Group Exercise 1 [Sorting]/3_sorting.cpp
@@ -26,6 +26,9 @@ void swap(int* a, int* b);
 int partition (int arr[], int low, int high);
 void quickSort(int arr[], int low, int high);
 
+//comment
+
+
 // ==================== MAIN WORKSPACE ==================================================
 
 int main() {
