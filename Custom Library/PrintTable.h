dd91614e714da6aa3160601b1a7461a25d52ebef
Requiem5032 <public_receiver035@pm.me>
2022-04-25 15:09:21 +0700

    Update PrintTable.h

--- Custom Library/PrintTable.h
+++ Custom Library/PrintTable.h
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <string>
+#include <math.h>
 
 void printTable(int r, int c, int w)
 {
-    std::string s;
+    std::string temp;
     int space;
 
     for (int i = 0; i < r; ++i)
@@ -22,8 +23,8 @@ void printTable(int r, int c, int w)
 
         for (int j = 0; j < c; ++j)
         {
-            std::cin >> s;
-            space = (w - s.size()) / 2;
+            std::cin >> temp;
+            space = floor((w - temp.size()) / 2);
             std::cout << "|";
 
             for (int k = 0; k < space; ++k)
@@ -31,9 +32,9 @@ void printTable(int r, int c, int w)
                 std::cout << " ";
             }
 
-            std::cout << s;
+            std::cout << temp;
 
-            for (int k = 0; k < w - (int)s.size() - space; ++k)
+            for (int k = 0; k < w - (int)temp.size() - space; ++k)
             {
                 std::cout << " ";
             }
@@ -77,7 +78,7 @@ void printTable(std::string* array, int r, int c, int w)
         for (int j = 0; j < c; ++j)
         {
             temp = array[i * c + j];
-            space = (w - temp.size()) / 2;
+            space = floor((w - temp.size()) / 2);
             std::cout << "|";
 
             for (int k = 0; k < space; ++k)
@@ -132,7 +133,7 @@ void printTable(std::string** array, int r, int c, int w)
         for (int j = 0; j < c; ++j)
         {
             temp = array[i][j];
-            space = (w - temp.size()) / 2;
+            space = floor((w - temp.size()) / 2);
             std::cout << "|";
 
             for (int k = 0; k < space; ++k)
